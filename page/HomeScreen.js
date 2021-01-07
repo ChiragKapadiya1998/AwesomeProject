fb23ca89833e2648b5986bb7ad34cdf6c72387c9
Chirag <51257430+chikskapadiya@users.noreply.github.com>
2021-01-07 16:48:44 +0530

    fix:add box

--- page/HomeScreen.js
+++ page/HomeScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';  
-import { View, Text, TextInput,Button,StyleSheet } from 'react-native';  
+import { View, Text, TextInput,Button,StyleSheet ,Platform} from 'react-native';  
 import { ScrollView } from 'react-native-gesture-handler';
 
 
@@ -9,7 +9,7 @@ import { ScrollView } from 'react-native-gesture-handler';
             <ScrollView>
                 <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>  
                     <Text>Home Screen</Text> 
-                    <Button title='Go To ListScreen ' onPress={() => navigation.navigate('List')}/>
+                    <Button style={styles.butto} title='Go To ListScreen ' onPress={() => navigation.navigate('List')}/>
                     <Button title='Go To Buttonscreen' onPress={() => navigation.navigate('Buttons')}/>
                     <Button title='Go To ImageScreen' onPress={() => navigation.navigate('Img')}/>
                     <Button title='Go To DemoScreen' onPress={() => navigation.navigate('DemoScreen')}/>
@@ -18,10 +18,15 @@ import { ScrollView } from 'react-native-gesture-handler';
                     <Button title='Go To SquareScreen' onPress={() => navigation.navigate('SquareScreen')}/>    
                     <Button title='Go To ReducerScreen' onPress={() => navigation.navigate('ReducerScreen')}/>    
                     <Button title='Go To Datafetch' onPress={() => navigation.navigate('Datafetch')}/>    
+                    <Button title='Go To TextScreen' onPress={() => navigation.navigate('TextScreen')}/>    
+                    <Button title="Go to Boxscreen" onPress={()=>navigation.navigate('BoxScreen')} />
                 </View>  
             </ScrollView>
         );  
  
 }  
+const styles = StyleSheet.create({
+    
+})
 
 export default HomeScreen;
